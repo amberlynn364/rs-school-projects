a2138ef0ff2ee604a6b478facbe17d5771992b7f
amberlynn <amberlynn364@gmail.com>
2023-06-10 02:19:07 +0300

    refactor: migrate sources to TS

--- migration-to-typescript/src/components/view/sources/sources.ts
+++ migration-to-typescript/src/components/view/sources/sources.ts
@@ -1,20 +1,23 @@
 import './sources.css';
+import { sourceNews } from '../../../types/index';
 
 class Sources {
-    draw(data) {
-        const fragment = document.createDocumentFragment();
-        const sourceItemTemp = document.querySelector('#sourceItemTemp');
+    draw(data: sourceNews[]) {
+        const fragment: DocumentFragment = document.createDocumentFragment();
+        const sourceItemTemp: HTMLTemplateElement | null = document.querySelector('#sourceItemTemp');
 
-        data.forEach((item) => {
-            const sourceClone = sourceItemTemp.content.cloneNode(true);
-
-            sourceClone.querySelector('.source__item-name').textContent = item.name;
-            sourceClone.querySelector('.source__item').setAttribute('data-source-id', item.id);
-
-            fragment.append(sourceClone);
+        data.forEach((item: sourceNews) => {
+            if (sourceItemTemp) {
+                const sourceClone: Node = sourceItemTemp.content.cloneNode(true) as HTMLElement;
+                if (sourceClone instanceof DocumentFragment) {
+                    sourceClone.querySelector('.source__item-name')!.textContent = item.name;
+                    sourceClone.querySelector('.source__item')!.setAttribute('data-source-id', item.id);
+                }
+                fragment.append(sourceClone);
+            }
         });
 
-        document.querySelector('.sources').append(fragment);
+        document.querySelector('.sources')!.append(fragment);
     }
 }
 
