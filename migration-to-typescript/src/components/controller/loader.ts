2c20c29a7a524b18690917d6d8446b60f6137c49
amberlynn <amberlynn364@gmail.com>
2023-06-11 00:19:48 +0300

    refactor: migrate loader to TS

--- migration-to-typescript/src/components/controller/loader.ts
+++ migration-to-typescript/src/components/controller/loader.ts
@@ -1,19 +1,20 @@
+import { LoaderOptions, GetResp, MethodsAppView } from '../../types/index';
 class Loader {
-    constructor(baseLink, options) {
+    constructor(private baseLink: string, private options: LoaderOptions) {
         this.baseLink = baseLink;
         this.options = options;
     }
 
-    getResp(
-        { endpoint, options = {} },
-        callback = () => {
+    public getResp(
+        { endpoint, options = {} }: GetResp,
+        callback = (): void => {
             console.error('No callback for GET response');
         }
-    ) {
+    ): void {
         this.load('GET', endpoint, callback, options);
     }
 
-    errorHandler(res) {
+    private errorHandler(res: Response): Response {
         if (!res.ok) {
             if (res.status === 401 || res.status === 404)
                 console.log(`Sorry, but there is ${res.status} error: ${res.statusText}`);
@@ -23,23 +24,23 @@ class Loader {
         return res;
     }
 
-    makeUrl(options, endpoint) {
-        const urlOptions = { ...this.options, ...options };
-        let url = `${this.baseLink}${endpoint}?`;
+    private makeUrl(options: LoaderOptions | {}, endpoint: string): string {
+        const urlOptions: LoaderOptions = { ...this.options, ...options };
+        let url: string = `${this.baseLink}${endpoint}?`;
 
-        Object.keys(urlOptions).forEach((key) => {
-            url += `${key}=${urlOptions[key]}&`;
+        Object.keys(urlOptions).forEach((key: string): void => {
+            url += `${key}=${urlOptions[key as keyof typeof urlOptions]}&`;
         });
 
         return url.slice(0, -1);
     }
 
-    load(method, endpoint, callback, options = {}) {
+    protected load<T>(method: string, endpoint: string, callback: (data: T) => void, options = {}): void {
         fetch(this.makeUrl(options, endpoint), { method })
-            .then(this.errorHandler)
-            .then((res) => res.json())
-            .then((data) => callback(data))
-            .catch((err) => console.error(err));
+        .then(this.errorHandler)
+            .then((res: Response): Promise<T> => res.json())
+            .then((data: T): void => callback(data))
+            .catch((err: string): void => console.error(err));
     }
 }
 
