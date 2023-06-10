c3bb53b448bebfe3f5e9f5d92bd1f2869d2ae214
amberlynn <amberlynn364@gmail.com>
2023-06-11 02:08:44 +0300

    refactor: migrate app to TS

--- migration-to-typescript/src/components/app/app.ts
+++ migration-to-typescript/src/components/app/app.ts
@@ -1,17 +1,20 @@
 import AppController from '../controller/controller';
 import { AppView } from '../view/appView';
+import { MethodsAppView } from '../../types/index';
 
 class App {
+    private controller
+    private view
     constructor() {
         this.controller = new AppController();
         this.view = new AppView();
     }
 
-    start() {
+    public start(): void {
         document
-            .querySelector('.sources')
-            .addEventListener('click', (e) => this.controller.getNews(e, (data) => this.view.drawNews(data)));
-        this.controller.getSources((data) => this.view.drawSources(data));
+            .querySelector('.sources')!
+            .addEventListener('click', (e: Event): void => this.controller.getNews(e as MouseEvent, (data): void => this.view.drawNews(data as MethodsAppView)));
+        this.controller.getSources((data): void => this.view.drawSources(data as MethodsAppView));
     }
 }
 
