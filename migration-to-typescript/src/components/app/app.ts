ab3d785de0299a79f7b0dad364e1cdf8738d2eec
amberlynn <amberlynn364@gmail.com>
2023-06-12 16:19:23 +0300

    refactor: replace keyword "as" with using type assertions in app.ts

--- migration-to-typescript/src/components/app/app.ts
+++ migration-to-typescript/src/components/app/app.ts
@@ -3,20 +3,22 @@ import { AppView } from '../view/appView';
 import { MethodsAppView } from '../../types/index';
 
 class App {
-    private controller;
-    private view;
-    constructor() {
-        this.controller = new AppController();
-        this.view = new AppView();
-    }
+    private controller = new AppController();
+    private view = new AppView();
 
     public start(): void {
-        document
-            .querySelector('.sources')!
-            .addEventListener('click', (e: Event): void =>
-                this.controller.getNews(e as MouseEvent, (data): void => this.view.drawNews(data as MethodsAppView))
-            );
-        this.controller.getSources((data): void => this.view.drawSources(data as MethodsAppView));
+        document.querySelector('.sources')!.addEventListener('click', (e: Event): void =>
+            this.controller.getNews<MethodsAppView>(e as MouseEvent, (data): void => {
+                if (data !== undefined) {
+                    this.view.drawNews(data);
+                }
+            })
+        );
+        this.controller.getSources<MethodsAppView>((data): void => {
+            if (data !== undefined) {
+                this.view.drawSources(data);
+            }
+        });
     }
 }
 
