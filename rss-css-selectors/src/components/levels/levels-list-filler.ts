6f269fb9673495bcc2a16e110b8d9f12d34d4c1b
amberlynn <amberlynn364@gmail.com>
2023-07-04 02:13:03 +0300

    feat: implement Level-list-filler class

---
+++ rss-css-selectors/src/components/levels/levels-list-filler.ts
@@ -0,0 +1,35 @@
+import { ElementObject } from '../../types/types';
+import ElementCreator from '../helpers/element-creator';
+
+export default class LevelsListFiller {
+  private levelsListWrapper = document.querySelector('.levels-list');
+
+  private parentElement: HTMLElement | null;
+
+  private childElement: HTMLElement | null;
+
+  constructor(arr: any[]) {
+    this.levelsListFiller(arr);
+  }
+
+  levelsListFiller(arr: any[]) {
+    this.levelsListWrapper!.innerHTML = '';
+    arr.forEach((item) => {
+      const elemParent: ElementObject = {
+        tag: item.tag,
+        classNames: item.classList,
+        textContent: item.textContent,
+      };
+
+      const elemChild: ElementObject = {
+        tag: item.childTag,
+        classNames: item.childClassList,
+        textContent: '',
+      };
+      this.parentElement = new ElementCreator(elemParent).getElement();
+      this.childElement = new ElementCreator(elemChild).getElement();
+      this.parentElement!.append(this.childElement as HTMLElement);
+      this.levelsListWrapper!.append(this.parentElement as HTMLElement);
+    });
+  }
+}
