7a1026be2a11ff57f28241939befa209a52df8e6
amberlynn <amberlynn364@gmail.com>
2023-07-04 02:10:08 +0300

    feat: add burger

---
+++ rss-css-selectors/src/components/burger-menu/burger-menu.ts
@@ -0,0 +1,42 @@
+export default class BurgerMenu {
+  private burgerMenu: HTMLDivElement | null = document.querySelector('.burger');
+  
+  private levels = document.querySelectorAll('.levels-item');
+
+  private overlay: HTMLDivElement | null = document.querySelector('.burger-overlay');
+
+  constructor() {
+    // this.burgerMenu!.addEventListener('click', () => this.toggleMenu());
+    // this.levels!.forEach((level) => level.addEventListener('click', () => {
+    //   if (this.burgerMenu!.classList.contains('active')) {
+    //     this.toggleMenu();
+    //   }
+    // }));
+    // this.overlay!.addEventListener('click', () => this.toggleMenu());
+    this.initEventListener();
+  }
+
+  private initEventListener(): void {
+    this.burgerMenu!.addEventListener('click', () => this.toggleMenu());
+    this.levels!.forEach((level) => level.addEventListener('click', () => {
+      if (this.burgerMenu!.classList.contains('active')) {
+        this.toggleMenu();
+      }
+    }));
+    this.overlay!.addEventListener('click', () => this.toggleMenu());
+  }
+
+  private toggleMenu(): void {
+    const body: HTMLElement | null = document.body;
+    const levelsWrapper: HTMLElement | null = document.querySelector('.right-side');
+    this.burgerMenu!.classList.toggle('active');
+
+    if (this.burgerMenu!.classList.contains('active')) {
+      body.style.overflow = 'hidden';
+      levelsWrapper!.style.right = '0';
+    } else {
+      body.style.overflow = 'visible';
+      levelsWrapper!.style.right = '-999px';
+    }
+  }
+}
