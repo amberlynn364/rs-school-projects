2811faaa937574a1ba8d997362cb425daa8aa351
amberlynn <amberlynn364@gmail.com>
2023-07-04 02:16:12 +0300

    refactor: add main classes to script.ts

--- rss-css-selectors/src/script.ts
+++ rss-css-selectors/src/script.ts
@@ -1,3 +1,12 @@
 import './style.scss';
 import './index.html';
+import LevelChanger from './components/levels/level-changer';
+import NewGame from './components/new-game/new-game';
+import BurgerMenu from './components/burger-menu/burger-menu';
+
+new LevelChanger();
+
+new NewGame();
+
+new BurgerMenu();
 
