25af3615978c44e6cb4465ea04c8112073966b2e
amberlynn <amberlynn364@gmail.com>
2023-07-06 19:09:45 +0300

    feat: implement new set-level class

---
+++ rss-css-selectors/src/components/levels/set-level.ts
@@ -0,0 +1,22 @@
+import LevelsHelper from '../helpers/levels-helper';
+import MyLocalStorage from '../helpers/my-local-storage';
+import MarkUp from '../markup/markup';
+import Table from '../table/table';
+import TextContent from '../text-content/text-content';
+import { levels } from './levels';
+
+export default class SetLevel {
+  private levelsItem: NodeListOf<HTMLLIElement> = document.querySelectorAll('.levels-item');
+
+  constructor(lvl: number) {
+    this.setLevel(lvl);
+  }
+
+  public setLevel(lvl: number): void {
+    new Table(lvl);
+    new MarkUp(lvl);
+    new TextContent(lvl);
+    LevelsHelper.highlightLevel(lvl, this.levelsItem);
+    MyLocalStorage.setItem('level', levels[lvl - 1].levelNumber);
+  }
+}
