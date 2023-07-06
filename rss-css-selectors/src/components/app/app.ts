6743e3e3a336c31ba91d6e992c39d85496a35f58
amberlynn <amberlynn364@gmail.com>
2023-07-06 22:36:29 +0300

    feat: implement App class

---
+++ rss-css-selectors/src/components/app/app.ts
@@ -0,0 +1,8 @@
+import AppController from '../controllers/app-controller';
+
+export default class App {
+  
+  start() {
+    new AppController();
+  }
+}
--- rss-css-selectors/src/script.ts
+++ rss-css-selectors/src/script.ts
@@ -1,18 +1,7 @@
 import './style.scss';
 import './index.html';
-import LevelChanger from './components/levels/level-changer';
-import NewGame from './components/new-game/new-game';
-import BurgerMenu from './components/burger-menu/burger-menu';
-import LevelsListFiller from './components/levels/levels-list-filler';
-import LevelsHelper from './components/helpers/levels-helper';
+import App from './components/app/app';
 
-new LevelsListFiller(LevelsHelper.getLevelsList());
 
-new LevelsHelper();
-
-new LevelChanger();
-
-// new NewGame();
-
-new BurgerMenu();
-
+const app = new App();
+app.start();
