3d6b60bef42a2a4f7f97dc81bd6546074f669c2e
amberlynn <amberlynn364@gmail.com>
2023-07-23 20:42:40 +0300

    refactor: update popup logic

--- async-race/src/app/winner-popup-handlers/winner-popup-handlers.ts
+++ async-race/src/app/winner-popup-handlers/winner-popup-handlers.ts
@@ -4,7 +4,8 @@ const closePopupButton: HTMLButtonElement | null = document.querySelector('#clos
 const popupOverlay: HTMLElement | null = document.querySelector('#popup-overlay');
 
 export function showPopupAndTextWinner(carWinner: string, raceTime: string): void {
-  if (winnerPopup && winnerMessage) {
+  const raceButton: HTMLElement | null = document.querySelector('#race-button');
+  if (winnerPopup && winnerMessage && raceButton?.hasAttribute('disabled')) {
     winnerPopup.classList.toggle('hide');
     winnerMessage.textContent = `${carWinner} won the race in ${raceTime} seconds`;
     popupOverlay!.style.display = 'block';
