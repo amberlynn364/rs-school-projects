d388d987ada3fe549eb5ae690158856a8dab7520
amberlynn <amberlynn364@gmail.com>
2023-07-23 20:39:50 +0300

    refactor: remove duplication, moved to a separate logic

--- async-race/src/app/garage-handler/garage-button-handlers.ts
+++ async-race/src/app/garage-handler/garage-button-handlers.ts
@@ -1,29 +1,42 @@
-import { Car, CreateOrUpdate, PaginationButtons, RemoveOrSelectButtons } from '../../types/types';
+import {
+  Car,
+  CarData,
+  CreateOrUpdate,
+  EngineResource,
+  ItemLimit,
+  PaginationButtons,
+  RemoveOrSelectButtons,
+  Urls,
+} from '../../types/types';
+import { startCarAnimation, stopCarAnimation } from '../car-handler/car-animation';
 import { renderCars } from '../car-handler/render-cars';
-import { updateCarsCounter } from '../car-handler/update-cars-counter';
 import { disableElement } from '../helpers/disable-element';
 import { enableElement } from '../helpers/enable-element';
-import { getCarIDFromButton } from '../helpers/get-car-id-from-button';
+import { getIDFromElement } from '../helpers/get-id-from-element';
+import { getDistanceBetweenTwoElements } from '../helpers/get-element-positions';
 import { getRandomCarName } from '../helpers/get-random-car-name';
 import { getRandomColor } from '../helpers/get-random-color';
 import { handleShakeAnimation } from '../helpers/handle-shake-animation';
+import { startCarEngine, stopCarEngine, switchCarEngineToDriveMode } from './garage-car-engine-api-handler';
 import {
-  createCarOnServer,
-  deleteCarFromServer,
-  fetchCarFromServer,
-  getTotalCarNumber,
-  updateCarDataOnServer,
-} from './garage-api-handlers';
+  createItemOnServer,
+  deleteItemOnServer,
+  fetchDataFromServerWithLimit,
+  fetchItemFromServer,
+  getTotalItemsFromServer,
+  updateItemOnServer,
+} from '../api-requests/api-requests';
+import { updateView } from '../helpers/update-view';
 
 const createTextInput: HTMLInputElement | null = document.querySelector('#create-text-input');
 const createColorInput: HTMLInputElement | null = document.querySelector('#create-color-input');
 const updateTextInput: HTMLInputElement | null = document.querySelector('#update-text-input');
 const updateColorInput: HTMLInputElement | null = document.querySelector('#update-color-input');
 
-const firstPageButton: HTMLButtonElement | null = document.querySelector('#first-page-button');
-const prevPageButton: HTMLButtonElement | null = document.querySelector('#prev-page-button');
-const nextPageButton: HTMLButtonElement | null = document.querySelector('#next-page-button');
-const lastPageButton: HTMLButtonElement | null = document.querySelector('#last-page-button');
+const firstPageButton: HTMLButtonElement | null = document.querySelector('#first-page-button-garage');
+const prevPageButton: HTMLButtonElement | null = document.querySelector('#prev-page-button-garage');
+const nextPageButton: HTMLButtonElement | null = document.querySelector('#next-page-button-garage');
+const lastPageButton: HTMLButtonElement | null = document.querySelector('#last-page-button-garage');
 
 const FIRST_PAGE_NUMBER = 1;
 const LIMIT_CARS_ON_PAGE = 7;
@@ -43,14 +56,12 @@ export function handleCarManagerButtonClick(mode: CreateOrUpdate): void {
       handleShakeAnimation(wrapperSelector);
     } else {
       if (mode === 'create') {
-        createCarOnServer({ name: carName, color: carColor }).then(() => {
-          renderCars();
-          updateCarsCounter();
-          disablePaginationButtons();
+        createItemOnServer<Car>(Urls.garage, { name: carName, color: carColor }).then(() => {
+          updateView('garage');
         });
         createTextInput!.value = '';
       } else {
-        updateCarDataOnServer({ name: carName, color: carColor }, carID)
+        updateItemOnServer<Car>(Urls.garage, { name: carName, color: carColor }, carID)
           .then(() => renderCars())
           .then(() => disableElement([...document.querySelector(wrapperSelector)!.children] as HTMLElement[]));
       }
@@ -63,36 +74,35 @@ export function handleGenerateCarsButtonClick() {
     const carName = getRandomCarName();
     const carColor = getRandomColor();
 
-    createCarOnServer({ name: carName, color: carColor });
+    createItemOnServer<Car>(Urls.garage, { name: carName, color: carColor });
   }
 
-  renderCars();
-  updateCarsCounter();
+  updateView('garage');
 }
 
 export function handleCarButtonClick(target: HTMLElement, buttonType: RemoveOrSelectButtons): void {
   if (buttonType === 'remove-button') {
-    carID = getCarIDFromButton(target);
-    deleteCarFromServer(carID).then(() => {
+    carID = getIDFromElement(target);
+    deleteItemOnServer(Urls.garage, carID).then(() => {
       const carContainer = document.querySelector('.car-container');
       if (carContainer!.children.length === 1) {
         pageNumber -= 1;
-        renderCars();
-        updateCarsCounter();
-        disablePaginationButtons();
+        updateView('garage');
       } else {
-        renderCars();
-        updateCarsCounter();
-        disablePaginationButtons();
+        updateView('garage');
       }
+    });
+
+    deleteItemOnServer(Urls.winners, carID).then(() => {
+      updateView('winners');
     });
   }
 
   if (buttonType === 'select-button') {
-    carID = getCarIDFromButton(target);
+    carID = getIDFromElement(target);
     const updateCarWrapper = document.querySelector('.update-car-wrapper');
     enableElement([...updateCarWrapper!.children] as HTMLElement[]);
-    fetchCarFromServer(carID).then((carData: Car) => {
+    fetchItemFromServer<Car>(Urls.garage, carID).then((carData) => {
       updateTextInput!.value = carData.name;
       updateColorInput!.value = carData.color;
     });
@@ -102,33 +112,33 @@ export function handleCarButtonClick(target: HTMLElement, buttonType: RemoveOrSe
 export function handlePaginationButtonClick(buttonType: PaginationButtons) {
   switch (buttonType) {
     case 'first-page-button':
-      pageNumber = 1;
-      renderCars();
-      disablePaginationButtons();
+      updatePageNumberAndRenderCars(1);
       break;
     case 'prev-page-button':
-      pageNumber -= 1;
-      renderCars();
-      disablePaginationButtons();
+      updatePageNumberAndRenderCars(pageNumber - 1);
       break;
     case 'next-page-button':
-      pageNumber += 1;
-      renderCars();
-      disablePaginationButtons();
+      updatePageNumberAndRenderCars(pageNumber + 1);
       break;
     case 'last-page-button':
-      getTotalCarNumber().then((number) => {
-        const lastPage = Math.round(number / LIMIT_CARS_ON_PAGE);
-        pageNumber = lastPage;
-        renderCars();
-        disablePaginationButtons();
+      getTotalItemsFromServer(Urls.garage).then((number) => {
+        const lastPage = Math.ceil(number / LIMIT_CARS_ON_PAGE);
+        updatePageNumberAndRenderCars(lastPage);
       });
   }
 }
 
-function disablePaginationButtons() {
+function updatePageNumberAndRenderCars(newPageNumber: number) {
+  const pageNumberElement: HTMLElement | null = document.querySelector('#page-number');
+  pageNumber = newPageNumber;
+  pageNumberElement!.textContent = `Page #${pageNumber}`;
+  renderCars();
+  disableGaragePaginationButtons();
+}
+
+export function disableGaragePaginationButtons() {
   if (firstPageButton && prevPageButton && nextPageButton && lastPageButton) {
-    getTotalCarNumber().then((number) => {
+    getTotalItemsFromServer(Urls.garage).then((number) => {
       const lastPage = Math.ceil(number / LIMIT_CARS_ON_PAGE);
       if (pageNumber === FIRST_PAGE_NUMBER) {
         disableElement([firstPageButton, prevPageButton]);
@@ -144,4 +154,42 @@ function disablePaginationButtons() {
   }
 }
 
-disablePaginationButtons();
+export function startEngineAndMoveCar(carId: number): void {
+  const carElement: HTMLElement | null = document.querySelector(`#car-id-${carId}`);
+  const finishFlag: HTMLElement | null = document.querySelector(`#flag-id-${carId}`);
+  startCarEngine(carId).then((resourceEngine: EngineResource) => {
+    if (carElement && finishFlag) {
+      const carVehicleSpeed: number = resourceEngine.velocity;
+      const carDistanceTraveled: number = resourceEngine.distance;
+      const travelTime: number = carDistanceTraveled / carVehicleSpeed;
+
+      const extraDistance = 10;
+      const distanceAnimation = Math.floor(getDistanceBetweenTwoElements(carElement, finishFlag)) + extraDistance;
+
+      startCarAnimation(carElement, distanceAnimation, travelTime, carId);
+
+      switchCarEngineToDriveMode(carId).then((drive) => {
+        if (!drive.success) {
+          stopCarAnimation(carId);
+        }
+      });
+    }
+  });
+}
+
+export function resetRaceHandler() {
+  const startButtons = document.querySelectorAll('[id^="start-button-id-"]');
+  const stopButtons = document.querySelectorAll('[id^="stop-button-id-"]');
+  disableElement(Array.from(stopButtons) as HTMLElement[]);
+  fetchDataFromServerWithLimit<CarData[]>(Urls.garage, pageNumber, ItemLimit.carsLimit)
+    .then((cars) => {
+      cars.forEach((car) =>
+        stopCarEngine(car.id).then(() => {
+          const vehicle: HTMLElement | null = document.querySelector(`#car-id-${car!.id}`);
+          vehicle!.style.transform = 'translateX(0px)';
+          stopCarAnimation(car.id);
+        })
+      );
+    })
+    .then(() => enableElement(Array.from(startButtons) as HTMLElement[]));
+}
--- async-race/src/app/garage-handler/garage-button-listeners.ts
+++ async-race/src/app/garage-handler/garage-button-listeners.ts
@@ -1,18 +1,31 @@
-import { RemoveOrSelectButtons } from '../../types/types';
+import { CarData, ItemLimit, RemoveOrSelectButtons, Urls } from '../../types/types';
+import { fetchDataFromServerWithLimit } from '../api-requests/api-requests';
+import { stopCarAnimation } from '../car-handler/car-animation';
+import { disableElement } from '../helpers/disable-element';
+import { enableElement } from '../helpers/enable-element';
+import { getIDFromElement } from '../helpers/get-id-from-element';
+import { hidePopup } from '../winner-popup-handlers/winner-popup-handlers';
 import {
   handleCarButtonClick,
   handleCarManagerButtonClick,
   handleGenerateCarsButtonClick,
   handlePaginationButtonClick,
+  pageNumber,
+  resetRaceHandler,
+  startEngineAndMoveCar,
 } from './garage-button-handlers';
+import { stopCarEngine } from './garage-car-engine-api-handler';
 
 const createCarButton: HTMLButtonElement | null = document.querySelector('#create-button');
 const updateCarButton: HTMLButtonElement | null = document.querySelector('#update-button');
 const generateCarsButton: HTMLButtonElement | null = document.querySelector('#generate-cars-button');
-const firstPageButton: HTMLButtonElement | null = document.querySelector('#first-page-button');
-const prevPageButton: HTMLButtonElement | null = document.querySelector('#prev-page-button');
-const nextPageButton: HTMLButtonElement | null = document.querySelector('#next-page-button');
-const lastPageButton: HTMLButtonElement | null = document.querySelector('#last-page-button');
+const raceButton: HTMLButtonElement | null = document.querySelector('#race-button');
+const resetRaceButton: HTMLButtonElement | null = document.querySelector('#reset-button');
+const firstPageButton: HTMLButtonElement | null = document.querySelector('#first-page-button-garage');
+const prevPageButton: HTMLButtonElement | null = document.querySelector('#prev-page-button-garage');
+const nextPageButton: HTMLButtonElement | null = document.querySelector('#next-page-button-garage');
+const lastPageButton: HTMLButtonElement | null = document.querySelector('#last-page-button-garage');
+const closePopupAndResetRace: HTMLButtonElement | null = document.querySelector('#close-and-reset-button');
 
 createCarButton!.addEventListener('click', () => handleCarManagerButtonClick('create'));
 updateCarButton!.addEventListener('click', () => handleCarManagerButtonClick('update'));
@@ -23,12 +36,58 @@ prevPageButton!.addEventListener('click', () => handlePaginationButtonClick('pre
 nextPageButton!.addEventListener('click', () => handlePaginationButtonClick('next-page-button'));
 lastPageButton!.addEventListener('click', () => handlePaginationButtonClick('last-page-button'));
 
+raceButton!.addEventListener('click', () => {
+  const startButtons = document.querySelectorAll('[id^="start-button-id-"]');
+  const stopButtons = document.querySelectorAll('[id^="stop-button-id-"]');
+  disableElement(raceButton!);
+  disableElement(Array.from(startButtons) as HTMLElement[]);
+  enableElement(Array.from(stopButtons) as HTMLElement[]);
+  enableElement(resetRaceButton!);
+  fetchDataFromServerWithLimit<CarData[]>(Urls.garage, pageNumber, ItemLimit.carsLimit).then((cars) => {
+    cars.forEach((car) => startEngineAndMoveCar(car.id));
+  });
+});
+
+resetRaceButton!.addEventListener('click', () => {
+  disableElement(resetRaceButton!);
+  resetRaceHandler();
+  enableElement(raceButton!);
+});
+closePopupAndResetRace!.addEventListener('click', () => {
+  hidePopup();
+  disableElement(resetRaceButton!);
+  resetRaceHandler();
+  enableElement(raceButton!);
+});
+
 document.addEventListener('click', (event) => {
   const { target } = event;
   if (target instanceof HTMLElement) {
     if (target.id.includes('remove-button') || target.id.includes('select-button')) {
       const buttonType: RemoveOrSelectButtons = target.id.includes('remove') ? 'remove-button' : 'select-button';
       handleCarButtonClick(target, buttonType);
+    }
+
+    if (target.id.includes('start-button')) {
+      const carID = getIDFromElement(target);
+      const stopEngineButton: HTMLElement | null = document.querySelector(`#stop-button-id-${carID}`);
+      startEngineAndMoveCar(carID);
+      enableElement(stopEngineButton!);
+      disableElement(target);
+    }
+
+    if (target.id.includes('stop-button')) {
+      const carID = getIDFromElement(target);
+      const car: HTMLElement | null = document.querySelector(`#car-id-${carID}`);
+      const startEngineButton: HTMLElement | null = document.querySelector(`#start-button-id-${carID}`);
+      target.textContent = 'Stopped';
+      stopCarEngine(carID).then(() => {
+        stopCarAnimation(carID);
+        car!.style.transform = 'translateX(0px)';
+        enableElement(startEngineButton!);
+        target.textContent = 'STOP';
+        disableElement(target);
+      });
     }
   }
 });
